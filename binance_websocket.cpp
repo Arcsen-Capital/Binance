405e27ce2eb606f4cb2da21fe55cd7cf58b665e1
Benedetto <s247082@studenti.polito.it>
2022-09-24 19:23:32 +0200

    binance library

---
+++ binance_websocket.cpp
@@ -0,0 +1,138 @@
+#include "binance_websocket.h"
+#include "binance_logger.h"
+
+map <struct lws *,CB> binance_websocket::handles ;
+
+
+
+//--------------------------
+int
+binance_websocket::event_cb( struct lws *wsi, enum lws_callback_reasons reason, void *user, void *in, size_t len )
+{
+
+    switch( reason )
+    {
+        case LWS_CALLBACK_CLIENT_ESTABLISHED:
+            lws_callback_on_writable( wsi );
+            break;
+
+        case LWS_CALLBACK_CLIENT_RECEIVE:
+
+            /* Handle incomming messages here. */
+            try {
+
+                //binance_logger::write_log("%p %s",  wsi, (char *)in );
+
+                string str_result = string( (char*)in );
+                Json::Reader reader;
+                Json::Value json_result;
+                reader.parse( str_result , json_result );
+
+                if ( handles.find( wsi ) != handles.end() ) {
+                    handles[wsi]( json_result );
+                }
+
+            } catch ( exception &e ) {
+                binance_logger::write_log( "<binance_websocket::event_cb> Error ! %s", e.what() );
+            }
+            break;
+
+        case LWS_CALLBACK_CLIENT_WRITEABLE:
+        {
+            break;
+        }
+
+        case LWS_CALLBACK_CLOSED:
+        case LWS_CALLBACK_CLIENT_CONNECTION_ERROR:
+            if ( handles.find( wsi ) != handles.end() ) {
+                handles.erase(wsi);
+            }
+            break;
+
+        default:
+            break;
+    }
+
+    return 0;
+}
+
+
+//-------------------
+void
+binance_websocket::init( )
+{
+    struct lws_context_creation_info info;
+    memset( &info, 0, sizeof(info) );
+
+    info.port = CONTEXT_PORT_NO_LISTEN;
+    info.protocols = &(this->protocols[0]);
+    info.gid = -1;
+    info.uid = -1;
+    info.options |= LWS_SERVER_OPTION_DO_SSL_GLOBAL_INIT;
+
+    context = lws_create_context(&info );
+}
+
+
+//----------------------------
+// Register call backs
+void
+binance_websocket::connect_endpoint (CB cb, const string& path)
+{
+    //char ws_path[1024];
+    //strcpy( ws_path, path.c_str() );
+
+    /* Connect if we are not connected to the server. */
+    struct lws_client_connect_info ccinfo = {0};
+    ccinfo.context 	= this->context;
+    ccinfo.address 	= BINANCE_WS_HOST;
+    ccinfo.port 	= BINANCE_WS_PORT;
+    ccinfo.path 	= path.c_str();
+    ccinfo.host 	= lws_canonical_hostname( context );
+    ccinfo.origin 	= "origin";
+    ccinfo.protocol = this->protocols[0].name;
+    ccinfo.ssl_connection = LCCSCF_USE_SSL | LCCSCF_ALLOW_SELFSIGNED | LCCSCF_SKIP_SERVER_CERT_HOSTNAME_CHECK;
+
+    struct lws* conn = lws_client_connect_via_info(&ccinfo);
+    handles[conn] = cb;
+
+
+}
+
+
+//----------------------------
+// Entering event loop
+void
+binance_websocket::enter_event_loop()
+{
+    while( 1 )
+    {
+        try {
+            lws_service( context, 500 );
+        } catch ( exception &e ) {
+            binance_logger::write_log( "<binance_websocket::enter_event_loop> Error ! %s", e.what() );
+            break;
+        }
+    }
+    lws_context_destroy( context );
+}
+
+binance_websocket::binance_websocket() {
+    this->context = nullptr;
+    this->protocols.push_back( {
+                                       "websocket",
+                                       binance_websocket::event_cb,
+                                       0,
+                                       65536,
+                               });
+    this->protocols.push_back({ NULL, NULL, 0, 0 });
+}
+
+binance_websocket::~binance_websocket() {
+    if(context)
+        lws_context_destroy( context );
+    context = nullptr;
+    binance_logger::close_file();
+}
+
+
---
+++ binance_websocket.h
@@ -0,0 +1,52 @@
+//
+// Created by james on 24/09/22.
+//
+
+#ifndef UNTITLED1_BINANCE_WEBSOCKET_H
+#define UNTITLED1_BINANCE_WEBSOCKET_H
+
+
+#include <cstring>
+#include <cstdlib>
+#include <cstdio>
+#include <cstdarg>
+
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <exception>
+
+#include <jsoncpp/json/json.h>
+#include <libwebsockets.h>
+
+
+#define BINANCE_WS_HOST "stream.binance.com"
+#define BINANCE_WS_PORT 9443
+
+
+using namespace std;
+
+typedef int (*CB)(Json::Value &json_value );
+
+
+class binance_websocket {
+
+private:
+    struct lws_context *context;
+    vector<struct lws_protocols> protocols;
+
+    static map <struct lws *,CB> handles ;
+
+public:
+    binance_websocket();
+    ~binance_websocket();
+    static int  event_cb( struct lws *wsi, enum lws_callback_reasons reason, void *user, void *in, size_t len );
+    void connect_endpoint(CB user_cb, const string& path);
+    void init();
+    void enter_event_loop();
+
+
+};
+
+#endif //UNTITLED1_BINANCE_WEBSOCKET_H
